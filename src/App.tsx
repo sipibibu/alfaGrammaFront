9821ecdea6e0685349aa9b246fb7ff12c6162973
RomanLeshchishin <romamail.ru365@gmail.com>
2023-10-29 13:59:10 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import axios from "axios";
+import {server} from "./const";
+
+function App() {
+  const [select, setSelect] = useState("Пользователь")
+  const getSelectedValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSelect(event.target.value)
+  }
+
+  const axiosGet = () => {
+      axios.get(server + "/register/respondent").then((response) => {
+          console.log(response);
+      });
+  }
+
+  return (
+    <>
+        <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
+        <p>Выберите роль</p>
+        <select onChange={() => getSelectedValue} value={select}>
+            <option>Пользователь</option>
+            <option>Предприниматель</option>
+        </select>
+        <button onClick={axiosGet}>войти</button>
+        </div>
+    </>
+  )
+}
+
+export default App
