c59e2ae96314d20055957586b32db7e6dd320890
Kirionenko <misha.kirionenko03@gmail.com>
2023-10-30 15:29:15 +0500

    Добавлено профиль пользователя

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -4,14 +4,18 @@ import classNames from 'classnames';
 export default function TopMenu() {
   return (
     <div className={styles.menu}>
-      <img src="/images/logo.png" className={styles.logo} alt="logo" />
+      <img
+        src="/src/components/TopMenu/logo.png"
+        className={styles.logo}
+        alt="logo"
+      />
       <nav className={styles.nav}>
         <a className={classNames(styles.link, styles.linkActive)}>опросы</a>
         <a className={styles.link}>мои опросы</a>
       </nav>
       <a href="#">
         <img
-          src="/images/profile-image.png"
+          src="/src/components/TopMenu/profile-image.png"
           className={styles.profileImage}
           alt="profile image"
         />
