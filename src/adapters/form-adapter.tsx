7345d2206ac78fb7f0464d767a387d4eaa46f13b
Kirionenko <misha.kirionenko03@gmail.com>
2023-12-24 19:27:04 +0500

    Добавлен адаптер для отправки форм на сервер

---
+++ src/adapters/form-adapter.tsx
@@ -0,0 +1,54 @@
+import { Gramma, Question, QuestionOptions, ScaleOptions } from '../types.ts';
+import { QuestionType } from '../const.ts';
+
+function adaptQuestionType(questionType: string) {
+  switch (questionType) {
+    default:
+    case QuestionType.Text:
+      return 'TextQuestion';
+    case QuestionType.Radio:
+      return 'RadioQuestion';
+    case QuestionType.Checkbox:
+      return 'CheckboxQuestion';
+    case QuestionType.Scale:
+      return 'ScaleQuestion';
+  }
+}
+
+function adaptOptions(questionType: string, options: QuestionOptions) {
+  switch (questionType) {
+    default:
+    case QuestionType.Text:
+      return [];
+    case QuestionType.Checkbox:
+    case QuestionType.Radio:
+      return options;
+    case QuestionType.Scale:
+      const scaleOptions = options as ScaleOptions;
+      return [
+        { text: scaleOptions.from },
+        { text: scaleOptions.to },
+        { text: scaleOptions.step },
+      ];
+  }
+}
+
+function adaptQuestion(question: Question) {
+  return {
+    questionText: question.title,
+    isReq: question.isRequired,
+    type: adaptQuestionType(question.type),
+    ansVar: adaptOptions(question.type, question.options),
+  };
+}
+
+export function adaptGramma(gramma: Gramma, companyId: number) {
+  return {
+    title: gramma.title,
+    fullDescription: gramma.description,
+    start: gramma.dateFrom,
+    end: gramma.dateTo,
+    questions: gramma.questions.map((question) => adaptQuestion(question)),
+    companyId,
+  };
+}
