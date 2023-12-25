119c531e8a5d27c88f35a5be9578697ccff619ae
Kirionenko <misha.kirionenko03@gmail.com>
2023-12-25 19:57:34 +0500

    Добавлен поиск по опросам

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,21 @@
-import * as React from 'react';
-import styles from './Search.module.css';
+import styles from "./Search.module.css";
 
-const Search = ({search, onChangeSearch}) => {
-    return (
-        <div className={styles.blockSearch}>
-            <input placeholder={"Поиск"} className={styles.inputSearch}
-                   value={search}
-                   onChange={e => onChangeSearch(e.target.value)}
-            />
-        </div>
-    );
+type searchProps = {
+  search: string;
+  onChangeSearch: (input: string) => void;
+};
+
+const Search = ({ search, onChangeSearch }: searchProps) => {
+  return (
+    <div className={styles.blockSearch}>
+      <input
+        placeholder={"Поиск"}
+        className={styles.inputSearch}
+        value={search}
+        onChange={(e) => onChangeSearch(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Search;
