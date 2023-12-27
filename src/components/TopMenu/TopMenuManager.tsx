d3e253ddbbc9f958b566bf1e15b91c66a3e94692
Kirionenko <misha.kirionenko03@gmail.com>
2023-12-28 00:54:47 +0500

    Обновлено верхнее меню

--- src/components/TopMenu/TopMenuManager.tsx
+++ src/components/TopMenu/TopMenuManager.tsx
@@ -6,25 +6,25 @@ import profileImage from "./profile-image.png";
 import { useStores } from "../../rootStoreContext.ts";
 
 const TopMenuManager = () => {
-    const { userStore } = useStores()
-    return (
-        <div className={styles.menu}>
-            <Link to={'/'}>
-                <img src={logo} className={styles.logo} alt="logo" />
-            </Link>
-            <nav className={styles.nav}>
-                <CustomLink link={"/"} text={"наши опросы"}/>
-                <CustomLink link={userStore.role == "Manager" ? '/gramma-constructor' : '/'} text={"создать опрос"}/>
-            </nav>
-            <Link to={userStore.role == "Manager" ? '/profile-manager' : '/'}>
-                <img
-                    src={profileImage}
-                    className={styles.profileImage}
-                    alt="profile image"
-                />
-            </Link>
-        </div>
-    );
+  const { userStore } = useStores();
+  return (
+    <div className={styles.menu}>
+      <Link to={"/"}>
+        <img src={logo} className={styles.logo} alt="logo" />
+      </Link>
+      <nav className={styles.nav}>
+        <CustomLink link={"/"} text={"наши опросы"} />
+        <CustomLink link={"/ourlist"} text={"создать опрос"} />
+      </nav>
+      <Link to={userStore.role == "Manager" ? "/profile-manager" : "/login"}>
+        <img
+          src={profileImage}
+          className={styles.profileImage}
+          alt="profile image"
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default TopMenuManager;
--- src/components/TopMenu/TopMenuRespondent.tsx
+++ src/components/TopMenu/TopMenuRespondent.tsx
@@ -1,22 +1,24 @@
-import styles from './topmenu.module.css';
-import logo from './logo.png';
-import profileImage from './profile-image.png';
-import { Link } from 'react-router-dom';
+import styles from "./topmenu.module.css";
+import logo from "./logo.png";
+import profileImage from "./profile-image.png";
+import { Link } from "react-router-dom";
 import CustomLink from "../UI/CustomLink.tsx";
 import { useStores } from "../../rootStoreContext.ts";
 
 export default function TopMenuRespondent() {
-  const { userStore} = useStores()
+  const { userStore } = useStores();
   return (
     <div className={styles.menu}>
-      <Link to={'/'}>
+      <Link to={"/"}>
         <img src={logo} className={styles.logo} alt="logo" />
       </Link>
       <nav className={styles.nav}>
-        <CustomLink link={"/"} text={"опросы"}/>
-        <CustomLink link={userStore.role == "Respondent" ? '/gramma-form' : '/'} text={"мои опросы"}/>
+        <CustomLink link={"/"} text={"опросы"} />
+        <CustomLink link={"/mylist"} text={"мои опросы"} />
       </nav>
-      <Link to={ userStore.role == "Respondent" ? '/profile-respondent' : '/'}>
+      <Link
+        to={userStore.role == "Respondent" ? "/profile-respondent" : "/login"}
+      >
         <img
           src={profileImage}
           className={styles.profileImage}
